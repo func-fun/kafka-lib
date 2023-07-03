4298898207ffd07b027d79401a84bd3c09a81f1c
Nick <nick.chiu@jobcloud.ch>
2023-07-03 14:01:06 +0200

    add linting

--- packages/kafka-avro-lib/index.d.ts
+++ packages/kafka-avro-lib/index.d.ts
@@ -1,31 +1,49 @@
-import { SchemaRegistry } from "@kafkajs/confluent-schema-registry";
-import { Kafka, Message, ProducerConfig, RecordMetadata } from "kafkajs";
-import { HeaderNames } from "./index";
+import { SchemaRegistry } from '@kafkajs/confluent-schema-registry';
+import { IHeaders, Kafka, Message, ProducerConfig, RecordMetadata } from 'kafkajs';
+import { HeaderNames } from './index';
 
 export interface TopicAvroSettings {
-    topicName: string,
-    keySubject: string,
-    payloadSubject: string,
-    keyVersion?: number,
-    payloadVersion?: number,
-    compression?: number,
+  topicName: string;
+  keySubject: string;
+  payloadSubject: string;
+  keyVersion?: number;
+  payloadVersion?: number;
+  compression?: number;
 }
 
-export type MessageHeaders = { [key in HeaderNames]?: string } | { [key: string]: any };
+export type MessageHeaders = { [key in HeaderNames]?: string } | IHeaders;
+export type MessagePayload = Record<string, string | number | boolean | Array<unknown> | Record<string, unknown>>;
 
 export class KafkaMessageHeaders {
-    public static getHeaderValue(headers: MessageHeaders, header: HeaderNames): string | null;
+  public static getHeaderValue(headers: MessageHeaders, header: HeaderNames): string | null;
 }
 
 export class AvroDecoder {
-    constructor(schemaRegistry: SchemaRegistry);
-    public decodeMessage(encodedKey: any, encodedPayload: any): Promise<any[]>;
+  constructor(schemaRegistry: SchemaRegistry);
+  // typing disabled, because it makes sense to comply with the return type of the 3rd party lib
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public decodeMessage(encodedKey: string, encodedPayload: string): Promise<any[]>;
 }
 
 export class AvroProducer {
-    constructor(kafka: Kafka, schemaRegistry: SchemaRegistry, topicAvroSettings: TopicAvroSettings[], config?: ProducerConfig|null);
-    public disconnect(): Promise<void>;
-    public produceMessage(key: string, payload: Object|null, topicName: string, headers?: MessageHeaders): Promise<RecordMetadata[]>;
-    public produceMessages(topicName: string, messages: Message[]): Promise<RecordMetadata[]>;
-    public produceSingleMessage(key: string, payload: Object|null, topicName: string, headers?: MessageHeaders): Promise<RecordMetadata[]>;
+  constructor(
+    kafka: Kafka,
+    schemaRegistry: SchemaRegistry,
+    topicAvroSettings: TopicAvroSettings[],
+    config?: ProducerConfig | null
+  );
+  public disconnect(): Promise<void>;
+  public produceMessage(
+    key: string,
+    payload: MessagePayload | null,
+    topicName: string,
+    headers?: MessageHeaders
+  ): Promise<RecordMetadata[]>;
+  public produceMessages(topicName: string, messages: Message[]): Promise<RecordMetadata[]>;
+  public produceSingleMessage(
+    key: string,
+    payload: MessagePayload | null,
+    topicName: string,
+    headers?: MessageHeaders
+  ): Promise<RecordMetadata[]>;
 }
--- packages/kafka-avro-lib/index.ts
+++ packages/kafka-avro-lib/index.ts
@@ -1,233 +1,203 @@
 import { SchemaRegistry } from '@kafkajs/confluent-schema-registry';
 import { Partitioners, Kafka, CompressionTypes, Message, Producer, RecordMetadata, ProducerConfig } from 'kafkajs';
-import { MessageHeaders, TopicAvroSettings } from '.';
+import { MessageHeaders, MessagePayload, TopicAvroSettings } from '.';
 
 export enum HeaderNames {
-    'Action' = 'Action',
-    'Authentication' = 'Authentication',
-    'Correlation-Id' = 'Correlation-Id',
-    'Expiry' = 'Expiry',
-    'Reply-To' = 'Reply-To',
-    'Type' = 'Type',
+  'Action' = 'Action',
+  'Authentication' = 'Authentication',
+  'Correlation-Id' = 'Correlation-Id',
+  'Expiry' = 'Expiry',
+  'Reply-To' = 'Reply-To',
+  'Type' = 'Type',
 }
 
 export enum Action {
-    'CREATE' = 'create',
-    'UPDATE' = 'update',
-    'DELETE' = 'delete'
+  'CREATE' = 'create',
+  'UPDATE' = 'update',
+  'DELETE' = 'delete',
 }
 
 class AvroDecoder {
-    private schemaRegistry: SchemaRegistry;
+  private schemaRegistry: SchemaRegistry;
 
-    constructor(schemaRegistry: SchemaRegistry) {
-        this.schemaRegistry = schemaRegistry;
+  constructor(schemaRegistry: SchemaRegistry) {
+    this.schemaRegistry = schemaRegistry;
+  }
+
+  // typing disabled, because it makes sense to comply with the return type of the 3rd party lib
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async decodeMessage(encodedKey: string, encodedPayload: string): Promise<any[]> {
+    if (null === encodedPayload || undefined === encodedPayload) {
+      return Promise.all([this.schemaRegistry.decode(Buffer.from(encodedKey, 'base64')), null]);
     }
 
-    public async decodeMessage(encodedKey: any, encodedPayload: any): Promise<any[]> {
-        if (null === encodedPayload || undefined === encodedPayload) {
-            return Promise.all(
-                [
-                    this.schemaRegistry.decode(Buffer.from(encodedKey, 'base64')),
-                    null
-                ]
-            );
-        }
-
-        return Promise.all(
-            [
-                this.schemaRegistry.decode(Buffer.from(encodedKey, 'base64')),
-                this.schemaRegistry.decode(Buffer.from(encodedPayload, 'base64'))
-            ]
-        );
-    }
+    return Promise.all([
+      this.schemaRegistry.decode(Buffer.from(encodedKey, 'base64')),
+      this.schemaRegistry.decode(Buffer.from(encodedPayload, 'base64')),
+    ]);
+  }
 }
 
 class AvroProducer {
-    private kafka: Kafka;
-    private producer: Producer;
-    private schemaRegistry: SchemaRegistry;
-    private topicAvroSettings: TopicAvroSettings[];
-    private defaultProducerConfig: ProducerConfig = { createPartitioner: Partitioners.DefaultPartitioner, idempotent: true }
+  private kafka: Kafka;
+  private producer: Producer;
+  private schemaRegistry: SchemaRegistry;
+  private topicAvroSettings: TopicAvroSettings[];
+  private defaultProducerConfig: ProducerConfig = {
+    createPartitioner: Partitioners.DefaultPartitioner,
+    idempotent: true,
+  };
 
-    constructor(
-        kafka: Kafka,
-        schemaRegistry: SchemaRegistry,
-        topicAvroSettings: TopicAvroSettings[],
-        config: ProducerConfig|null = null
-    ) {
-        config = config ?? this.defaultProducerConfig;
-        this.kafka = kafka;
-        this.producer = this.kafka.producer(config);
-        this.schemaRegistry = schemaRegistry;
-        this.topicAvroSettings = topicAvroSettings;
+  constructor(
+    kafka: Kafka,
+    schemaRegistry: SchemaRegistry,
+    topicAvroSettings: TopicAvroSettings[],
+    config: ProducerConfig | null = null
+  ) {
+    config = config ?? this.defaultProducerConfig;
+    this.kafka = kafka;
+    this.producer = this.kafka.producer(config);
+    this.schemaRegistry = schemaRegistry;
+    this.topicAvroSettings = topicAvroSettings;
+  }
+
+  public async disconnect(): Promise<void> {
+    await this.producer.disconnect();
+  }
+
+  public async produceMessage(
+    key: string,
+    payload: MessagePayload | null,
+    topicName: string,
+    headers?: MessageHeaders
+  ): Promise<RecordMetadata[]> {
+    const topicAvroSettings = this.getTopicAvroSettings(topicName);
+    const [encodedKey, encodedPayload] = await this.getEncodedMessage(key, payload, topicAvroSettings);
+
+    await this.producer.connect();
+
+    return this.producer.send({
+      topic: topicAvroSettings.topicName,
+      compression: topicAvroSettings.compression ?? CompressionTypes.GZIP,
+      messages: [this.getMessage(encodedKey, encodedPayload, headers)],
+    });
+  }
+
+  public async produceMessages(topicName: string, messages: Message[]): Promise<RecordMetadata[]> {
+    const topicAvroSettings = this.getTopicAvroSettings(topicName);
+    const encodedMessages: Message[] = await this.getEncodedMessages(messages, topicAvroSettings);
+
+    await this.producer.connect();
+    return this.producer.send({
+      topic: topicAvroSettings.topicName,
+      compression: topicAvroSettings.compression ?? CompressionTypes.GZIP,
+      messages: encodedMessages,
+    });
+  }
+
+  public async produceSingleMessage(
+    key: string,
+    payload: MessagePayload | null,
+    topicName: string,
+    headers?: MessageHeaders
+  ): Promise<RecordMetadata[]> {
+    const result = await this.produceMessage(key, payload, topicName, headers);
+    await this.disconnect();
+
+    return result;
+  }
+
+  private async getEncodedMessage(
+    key: string,
+    payload: MessagePayload | null,
+    topicAvroSettings: TopicAvroSettings
+  ): Promise<[Buffer, null] | [Buffer, Buffer]> {
+    const [keyId, payloadId] = await Promise.all([
+      this.getKeySchemaId(topicAvroSettings),
+      this.getPayloadSchemaId(topicAvroSettings),
+    ]);
+
+    if (null === payload) {
+      return Promise.all([this.schemaRegistry.encode(keyId, key), null]);
     }
 
-    public async disconnect(): Promise<void> {
-        await this.producer.disconnect();
+    return Promise.all([this.schemaRegistry.encode(keyId, key), this.schemaRegistry.encode(payloadId, payload)]);
+  }
+
+  private async getEncodedMessages(messages: Message[], topicConfig: TopicAvroSettings): Promise<Message[]> {
+    const [keyId, payloadId] = await Promise.all([
+      this.getKeySchemaId(topicConfig),
+      this.getPayloadSchemaId(topicConfig),
+    ]);
+
+    const encodedMessages: Promise<Message>[] = messages.map(async (message) => {
+      const [key, value] = await Promise.all([
+        this.schemaRegistry.encode(keyId, message.key),
+        null === message.value ? null : this.schemaRegistry.encode(payloadId, message.value),
+      ]);
+
+      if (undefined !== message.headers) {
+        return { key, value, headers: message.headers };
+      }
+
+      return { key, value };
+    });
+
+    return Promise.all(encodedMessages);
+  }
+
+  private getMessage(encodedKey: Buffer, encodedPayload: Buffer | null, headers?: MessageHeaders): Message {
+    if (undefined === headers) {
+      return {
+        key: encodedKey,
+        value: encodedPayload,
+      };
     }
 
-    public async produceMessage(
-        key: string,
-        payload: Object|null,
-        topicName: string,
-        headers?: MessageHeaders
-    ): Promise<RecordMetadata[]> {
-        var topicAvroSettings = this.getTopicAvroSettings(topicName);
-        const [encodedKey, encodedPayload] = await this.getEncodedMessage(key, payload, topicAvroSettings);
+    return {
+      key: encodedKey,
+      value: encodedPayload,
+      headers: headers,
+    };
+  }
 
-        await this.producer.connect();
-            
-        return this.producer.send(
-          {
-            topic: topicAvroSettings.topicName,
-            compression: topicAvroSettings.compression ?? CompressionTypes.GZIP,
-            messages: [
-              this.getMessage(encodedKey, encodedPayload, headers)
-            ]
-          }
-        );        
+  private async getKeySchemaId(topicConfig: TopicAvroSettings): Promise<number> {
+    return this.getSchemaId(topicConfig.keySubject, topicConfig.keyVersion);
+  }
+
+  private async getPayloadSchemaId(topicConfig: TopicAvroSettings): Promise<number> {
+    return this.getSchemaId(topicConfig.payloadSubject, topicConfig.payloadVersion);
+  }
+
+  private async getSchemaId(subject: string, version: number | undefined): Promise<number> {
+    if (undefined === version) {
+      return this.schemaRegistry.getLatestSchemaId(subject);
     }
 
-    public async produceMessages(
-        topicName: string,
-        messages: Message[]
-    ): Promise<RecordMetadata[]> {
-        var topicAvroSettings = this.getTopicAvroSettings(topicName);
-        let encodedMessages: Message[] = await this.getEncodedMessages(messages, topicAvroSettings);
+    return this.schemaRegistry.getRegistryId(subject, version);
+  }
 
-        await this.producer.connect();
-        return this.producer.send(
-          {
-            topic: topicAvroSettings.topicName,
-            compression: topicAvroSettings.compression ?? CompressionTypes.GZIP,
-            messages: encodedMessages
-          }
-        );
+  private getTopicAvroSettings(topicName: string): TopicAvroSettings {
+    const selectedSetting = this.topicAvroSettings.find((setting) => setting.topicName === topicName);
+
+    if (undefined === selectedSetting) {
+      throw `No avro settings for topic: ${topicName}`;
     }
 
-    public async produceSingleMessage(
-        key: string,
-        payload: Object|null,
-        topicName: string,
-        headers?: MessageHeaders
-    ): Promise<RecordMetadata[]> {
-        let result = await this.produceMessage(key, payload, topicName, headers);
-        await this.disconnect();
-
-        return result;
-    }
-
-    private async getEncodedMessage(
-        key: string,
-        payload: Object|null,
-        topicAvroSettings: TopicAvroSettings
-    ): Promise<[Buffer, null] | [Buffer, Buffer]> {
-        const [keyId, payloadId] = await Promise.all(
-            [
-                this.getKeySchemaId(topicAvroSettings),
-                this.getPayloadSchemaId(topicAvroSettings)
-            ]
-        );
-
-        if (null === payload) {
-            return Promise.all(
-                [
-                    this.schemaRegistry.encode(keyId, key),
-                    null
-                ]
-            );
-        }
-        
-        return Promise.all(
-            [
-                this.schemaRegistry.encode(keyId, key),
-                this.schemaRegistry.encode(payloadId, payload)
-            ]
-        );
-    }
-
-    private async getEncodedMessages(messages: Message[], topicConfig: TopicAvroSettings): Promise<Message[]> {
-        const [keyId, payloadId] = await Promise.all(
-            [
-                this.getKeySchemaId(topicConfig),
-                this.getPayloadSchemaId(topicConfig)
-            ]
-        );
-
-        const encodedMessages: Promise<Message>[] = messages.map(
-            async (message) => {
-                const [key, value] = await Promise.all(
-                    [
-                        this.schemaRegistry.encode(keyId, message.key),
-                        null === message.value ? null : this.schemaRegistry.encode(payloadId, message.value)
-                    ]
-                );
-
-                if (undefined !== message.headers) {
-                    return { key, value, headers: message.headers }
-                }
-
-                return { key, value };
-            }
-        )
-
-        return Promise.all(encodedMessages);
-    }
-
-    private getMessage(encodedKey: Buffer, encodedPayload: Buffer|null, headers?: MessageHeaders): Message {
-        if (undefined === headers) {
-            return {
-                key: encodedKey,
-                value: encodedPayload
-            };
-        }
-
-        return {
-            key: encodedKey,
-            value: encodedPayload,
-            headers: headers
-        };
-    }
-
-    private async getKeySchemaId(topicConfig: TopicAvroSettings): Promise<number> {
-        return this.getSchemaId(topicConfig.keySubject, topicConfig.keyVersion);
-    }
-
-    private async getPayloadSchemaId(topicConfig: TopicAvroSettings): Promise<number> {
-        return this.getSchemaId(topicConfig.payloadSubject, topicConfig.payloadVersion);
-    }
-
-    private async getSchemaId(subject: string, version: number | undefined): Promise<number> {
-        if (undefined === version) {
-            return this.schemaRegistry.getLatestSchemaId(subject);
-        }
-
-        return this.schemaRegistry.getRegistryId(subject, version);
-    }
-
-    private getTopicAvroSettings(topicName: string): TopicAvroSettings {
-        const selectedSetting =  this.topicAvroSettings.find(setting => setting.topicName === topicName);
-
-        if (undefined === selectedSetting) {
-            throw `No avro settings for topic: ${topicName}`;
-        }
-
-        return selectedSetting;
-    }
-
+    return selectedSetting;
+  }
 }
 
 class KafkaMessageHeaders {
-    public static getHeaderValue(headers: MessageHeaders[], header: HeaderNames|string): string | null {
-        const selected = headers.find(h => header in h );
-        
-        if (undefined === selected) {
-            return null;
-        }
+  public static getHeaderValue(headers: MessageHeaders[], header: HeaderNames | string): string | null {
+    const selected = headers.find((h) => header in h);
 
-        return Buffer.from(selected[header]).toString()
+    if (undefined === selected) {
+      return null;
     }
+
+    return Buffer.from(selected[header]).toString();
+  }
 }
 
-export { AvroProducer, AvroDecoder, KafkaMessageHeaders }
+export { AvroProducer, AvroDecoder, KafkaMessageHeaders };
