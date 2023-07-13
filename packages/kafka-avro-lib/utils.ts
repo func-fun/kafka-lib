be7311d301ddbf87da873c20cfb297cdf2f7094e
Nick <nick.chiu@jobcloud.ch>
2023-07-13 23:28:41 +0200

    fix linting

--- packages/kafka-avro-lib/utils.ts
+++ packages/kafka-avro-lib/utils.ts
@@ -1,11 +1,11 @@
 import { HeaderNames, MessageHeaders } from './types';
 
 export const getMessageHeaderValue = (headers: MessageHeaders[], header: HeaderNames | string): string | null => {
-  const selected = headers.find(h => header in h );
+  const selected = headers.find((h) => header in h);
 
   if (undefined === selected) {
-      return null;
+    return null;
   }
 
-  return Buffer.from(selected[header]).toString()
+  return Buffer.from(selected[header]).toString();
 };
