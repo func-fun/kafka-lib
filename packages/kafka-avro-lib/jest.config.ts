ac8018c38875a00e9344292b9cae39a8ad96e7ea
Nick Chiu <coding.nikazu@gmail.com>
2023-08-02 20:03:30 +0200

    add min cov

--- packages/kafka-avro-lib/jest.config.ts
+++ packages/kafka-avro-lib/jest.config.ts
@@ -3,6 +3,14 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 68,
+      functions: 62,
+      lines: 75,
+      statements: -18,
+    },
+  },
 };
 
 export default config;
